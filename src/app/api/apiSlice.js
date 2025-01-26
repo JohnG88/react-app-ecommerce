64cb1cfa305f1325f8bbb7b8bc8b394d63463816
John Gutierrez <djterez88@gmail.com>
2025-01-26 11:26:15 -0800

    Updated base url in apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials, logout } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
     // baseUrl: "http://localhost:8000",
-    baseUrl: "JohnG.pythonanywhere.com",
+    baseUrl: "johng.pythonanywhere.com",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;

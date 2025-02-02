4a00a52bc711c58f626b63340644160bd7bdb872
John Gutierrez <djterez88@gmail.com>
2025-02-01 19:34:51 -0800

    Updated ShippingAddress.jsx to not call API if no username is logged in.

--- src/features/billing-shipping/ShippingAddress.jsx
+++ src/features/billing-shipping/ShippingAddress.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUsername } from "../auth/authSlice";
 
 import {
     useGetBillingShippingQuery,
@@ -8,14 +10,19 @@ import {
 import AddressForm from "./AddressForm";
 
 const ShippingAddress = ({ formData, onFormDataChange }) => {
+    const username = useSelector(selectCurrentUsername);
+    const isAuthenticated = !!username;
+
     const {
         data: billingShipping,
         isLoading,
         isSuccess,
         isError,
-    } = useGetBillingShippingQuery("getBillingShipping");
+    } = useGetBillingShippingQuery("getBillingShipping", {
+        skip: !isAuthenticated,
+    });
 
-    const [initialized, setInitialized] = useState(false);
+    // const [initialized, setInitialized] = useState(false);
 
     const [updateBillingShippingAddress] =
         useUpdateBillingShippingAddressMutation();
@@ -23,41 +30,24 @@ const ShippingAddress = ({ formData, onFormDataChange }) => {
     // console.log("billingShipping", billingShipping);
 
     useEffect(() => {
-        if (!initialized && isSuccess) {
-            if (billingShipping && billingShipping.ids.length > 0) {
-                const id = billingShipping.ids[0];
-                if (id !== undefined && billingShipping.entities[id]) {
-                    const shippingBilling = billingShipping.entities[id];
-                    const initialData = {
-                        id: shippingBilling.id || "",
-                        firstName: shippingBilling.first_name || "",
-                        lastName: shippingBilling.last_name || "",
-                        email: shippingBilling.customer_detail.email || "",
-                        streetAddress: shippingBilling.street_address || "",
-                        apt: shippingBilling.apt || "",
-                        city: shippingBilling.city || "",
-                        state: shippingBilling.state || "",
-                        zipcode: shippingBilling.zipcode || "",
-                        isChecked: true,
-                    };
-                    onFormDataChange(initialData);
-                } else {
-                    // Handle case where the id exists but no entity is found
-                    onFormDataChange({
-                        id: "",
-                        firstName: "",
-                        lastName: "",
-                        email: "",
-                        streetAddress: "",
-                        apt: "",
-                        city: "",
-                        state: "",
-                        zipcode: "",
-                        isChecked: true,
-                    });
-                }
+        if (isAuthenticated && isSuccess && billingShipping?.ids?.length > 0) {
+            const id = billingShipping.ids[0];
+            if (id !== undefined && billingShipping.entities[id]) {
+                const shippingBilling = billingShipping.entities[id];
+                const initialData = {
+                    id: shippingBilling.id || "",
+                    firstName: shippingBilling.first_name || "",
+                    lastName: shippingBilling.last_name || "",
+                    email: shippingBilling.customer_detail?.email || "",
+                    streetAddress: shippingBilling.street_address || "",
+                    apt: shippingBilling.apt || "",
+                    city: shippingBilling.city || "",
+                    state: shippingBilling.state || "",
+                    zipcode: shippingBilling.zipcode || "",
+                    isChecked: true,
+                };
+                onFormDataChange(initialData);
             } else {
-                // Handle case where no address data is available
                 onFormDataChange({
                     id: "",
                     firstName: "",
@@ -71,16 +61,8 @@ const ShippingAddress = ({ formData, onFormDataChange }) => {
                     isChecked: true,
                 });
             }
-            setInitialized(true);
         }
-    }, [
-        initialized,
-        isSuccess,
-        billingShipping,
-        isLoading,
-        isError,
-        onFormDataChange,
-    ]);
+    }, [isAuthenticated, isSuccess, billingShipping, onFormDataChange]);
 
     let content;
     if (isLoading) {
